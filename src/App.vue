35e21afcafc0ef927605c483a2c89d7ab2d43277
AlexandreOttmann <ottmann.alex@gmail.com>
2023-06-19 15:08:17 +0200

    INIT: Initialize todo app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <todos></todos> 
+  </div>
+</template>    
+    
+<script>
+ import Todos from './components/Todos.vue'
+    
+ export default {
+      components: {
+        Todos
+      }
+    }
+    
+</script>
+
+
---
+++ src/components/Todos.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todos</h1>
+    <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addTodo"/>
+    </header>  
+    <div class="main">
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in todos" :class="{completed: todo.completed}">
+          <div class="view">
+            <input type="checkbox" class="toggle" v-model="todo.completed">
+            <label>{{ todo.name }}</label>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <footer class="footer">
+    <span class="todo-count">
+      <strong>{{ remaining }}</strong>
+      Tâche(s) restante(s)
+    </span>
+  </footer>
+</template>
+
+<script>
+export default {
+data (){
+  return {
+    todos: [{
+      name: 'Tâche de test',
+      completed: false
+    }],
+    newTodo: ''
+  }
+},
+methods: {
+  addTodo() {
+    this.todos.push({
+      completed: false,
+      name: this.newTodo
+    })
+    this.newTodo = ''
+  }
+},
+computed: {
+  remaining(){
+    return this.todos.filter(todo => !todo.completed).length
+  }
+}
+}
+
+</script>
+
+<style src="./todos.css">
+
+</style>
